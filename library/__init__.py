325250c54a79348f25b0c0097181f126720234a1
batboys45 <himmo4346@gmail.com>
2025-11-30 21:34:21 +0300

    update
    Icons have been updated, texts have been translated into Turkish, and some updates have been made.

--- library/__init__.py
+++ library/__init__.py
@@ -11,6 +11,7 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = "login_page"
 login_manager.login_message_category = "info"
+login_manager.login_message = ""  # Burayı ekledik, artık uyarı çıkmayacak.
 from library import routes
 
 with app.app_context():
--- library/forms.py
+++ library/forms.py
@@ -7,12 +7,12 @@ class RegisterForm(FlaskForm):
     def validate_username(self, username_to_check):
         user = User.query.filter_by(username=username_to_check.data).first()
         if user:
-            raise ValidationError('Username already exists! Please try a different username')
+            raise ValidationError('Kullanıcı adı alınmış! Lütfen farklı bir kullanıcı adı deneyin')
 
     def validate_email_address(self, email_address_to_check):
         email_address = User.query.filter_by(email_address=email_address_to_check.data).first()
         if email_address:
-            raise ValidationError('Email Address already exists! Please try a different email address')
+            raise ValidationError('Email adresi zaten kullanılıyor! Lütfen farklı bir email adresi deneyin')
 
     username = StringField(label='User Name:', validators=[Length(min=2, max=30), DataRequired()])
     email_address = StringField(label='Email Address:', validators=[Email(), DataRequired()])
@@ -28,8 +28,8 @@ class LoginForm(FlaskForm):
 
 # İSİM DEĞİŞİKLİĞİ: PurchaseItemForm -> BorrowBookForm
 class BorrowBookForm(FlaskForm):
-    submit = SubmitField(label='Borrow Book!')
+    submit = SubmitField(label='Ödünç al!')
 
 # İSİM DEĞİŞİKLİĞİ: SellItemForm -> ReturnBookForm
 class ReturnBookForm(FlaskForm):
-    submit = SubmitField(label='Return Book!')
+    submit = SubmitField(label='Geri ver!')
